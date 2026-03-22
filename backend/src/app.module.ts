920b7e9628f1cbc000fa071850b4261bce1fe3c8
Matheus Rodrigues <ec.mattrodrigues@gmail.com>
2026-03-22 02:19:55 -0300

    feat: generated prisma and adjusts prisma schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventController } from './event/event.controller';
+import { EventService } from './event/event.service';
+import { EventModule } from './event/event.module';
+import { PrismaController } from './prisma/prisma.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventModule, PrismaModule],
+  controllers: [AppController, EventController, PrismaController],
+  providers: [AppService, EventService, PrismaService],
 })
 export class AppModule {}
