5279c5b3f62773e3613e0f8402c89e9bd9866f3b
Matheus Rodrigues <ec.mattrodrigues@gmail.com>
2026-03-22 02:19:08 -0300

    feat: add event module

---
+++ backend/src/event/event.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { EventRepository } from './event.repository';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [EventService, EventRepository],
+  controllers: [EventController],
+  exports: [EventService],
+})
+export class EventModule {}
