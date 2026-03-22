d69483a1039c69b232be55b3091b829edf9dfab8
Matheus Rodrigues <ec.mattrodrigues@gmail.com>
2026-03-22 02:17:57 -0300

    feat: add event repository

---
+++ backend/src/event/event.repository.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+import { CreateEventDto } from './dto/create-event-dto';
+import { UpdateEventDto } from './dto/update-event-dto';
+import { Event } from '../../generated/prisma';
+
+@Injectable()
+export class EventRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createEventDto: CreateEventDto): Promise<Event> {
+    return await this.prismaService.event.create({
+      data: createEventDto,
+    });
+  }
+
+  async findAll(): Promise<Event[]> {
+    return await this.prismaService.event.findMany();
+  }
+
+  async findOne(id: number): Promise<Event> {
+    return await this.prismaService.event.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: number, updateEventDto: UpdateEventDto): Promise<Event> {
+    return await this.prismaService.event.update({
+      where: { id },
+      data: updateEventDto,
+    });
+  }
+
+  async remove(id: number): Promise<Event> {
+    return await this.prismaService.event.delete({
+      where: { id },
+    });
+  }
+}
