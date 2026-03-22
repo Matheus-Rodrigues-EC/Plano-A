30a0ccf4a63ab042733c31cb11add694d86bed9f
Matheus Rodrigues <ec.mattrodrigues@gmail.com>
2026-03-22 01:32:28 -0300

    feat: add updated-event-dto

---
+++ backend/src/event/dto/update-event-dto.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNotEmpty, IsString, IsDate } from 'class-validator';
+
+export class UpdateEventDto {
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  data!: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  local!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description!: string;
+}
